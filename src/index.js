9250d14dd877d62396159dee98c7b0de76f9d514
trehanpraharsh <trehanpraharsh@gmail.com>
2023-07-24 15:23:50 +0530

    Basic Structure

--- src/index.js
+++ src/index.js
@@ -50,11 +50,32 @@ const pizzaData = [
 function App() {
   return (
     <div>
-      <h1>Hello World!</h1>
+      <Header />
+      <Menu />
+      <Footer />
+    </div>
+  );
+}
+
+function Header() {
+  return <h1>Fast React Pizza Co.</h1>;
+}
+
+function Menu() {
+  return (
+    <div>
+      <h2>We Offer :-</h2>
+      <Pizza />
       <Pizza />
       <Pizza />
       <Pizza />
     </div>
+  );
+}
+
+function Footer() {
+  return (
+    <footer>{new Date().toLocaleTimeString()} - We're Currenty Open!</footer>
   );
 }
 
